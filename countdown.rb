cba78a5d6d023fc8304275ed0ed932ba45e63d85
Justin <jmp86@yahoo.com>
2021-08-11 17:41:28 -1000

    commit tree 51c50ca0341e1728bf00337dd7d5c457e0ce0f85

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    while number >= 1
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(integer)
+    while integer >= 1
+        integer -= 1
+        sleep(1)
+    end
 end
